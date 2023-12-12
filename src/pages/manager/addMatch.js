1fb44310621dcd4447002849b77bc21b6b250c85
Ahmed Emad <74673544+ahmedemad81@users.noreply.github.com>
2023-12-12 12:01:36 +0200

    Login Page / Add Match

--- src/pages/manager/addMatch.js
+++ src/pages/manager/addMatch.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Formik, Field, ErrorMessage, Form, FieldArray } from "formik";
+import { Formik, Field, Form } from "formik";
 import classes from "./form.module.css";
 import * as Yup from "yup";
-import axios from "axios";
+import axios from "../../requests/axios";
+import routes from "../../requests/routes";
 
 
 const AddMatch = (props) => {
+
+    const Teams = ['Al Ahly FC','Pyramids FC','Zamalek SC','Cleopatra FC','Modern Future FC','El Masry SC','Bank El Ahly','	Pharco FC','El Mokawloon SC',"Tala'ea El Gaish",'Ittihad Alexandria SC','Smouha SC','Enppi SC','Zed FC','	Ismaily SC','El Gouna FC',
+    'Baladiyat El Mahalla','El Dakhlia SC']
+
+    function dropDownMenu(option,index) {
+    return (
+        <option  key = {index}className={classes.option} value={index}>{option}</option>
+    )
+    }
 
     // Stadium Getting and Setting
     const [stadiumData,setStadiumData ] = useState([]);
@@ -35,7 +45,7 @@ const AddMatch = (props) => {
     }, []);
 
     const initialValues = {
-    venue:"",
+    stadium:"",
     firstTeam:"",
     secondTeam:"",
     date:"",
@@ -45,55 +55,53 @@ const AddMatch = (props) => {
     secondLinesmen:"",
     seating: [],
     noRows:"",
-    venuename:"",
+    stadiumname:"",
     reserved:0
     };
 
-    const validationSchema = Yup.object().shape({});
+    const validationSchema = Yup.object().shape({
+    stadium: Yup.string()
+        .required("Please enter a stadium"),
+        
+    });
 
     const onformSubmit = (data) => {
-
+    console.log(data);
     async function addMatch() {
-        // console.log(data.venue);
-        // const x = new Date('2023-01-01');
-        // const y = new Date(data.date);
-        // if(data.firstTeam==data.secondTeam)
-        // {
-        //     alert("Same team");
-        //     return;
-        // }
-        // else if(y<=x)
-        // {
-        //     alert("Past Date");
-        //     return;
-        // }
-        // try {
-        //     data.seating = Array.from({length: Number(stadiumData[Number(data.venue)].numSeats)}, (v, i) => 0);
-        //     data.noRows = stadiumData[Number(data.venue)].numRows;
-        //     data.venuename = stadiumData[Number(data.venue)].venue;
-        //     data.reserved = 0;
-        //     const request = await axios.post("http://localhost:3001/matches/add-match", data, 
-        //     { headers: { Authorization: `Bearer ${sessionStorage.getItem("tokenValue")}` } }).then((res) => {
-        //         console.log(res)
-        //     })
-        //     window.location.reload(false);
+       // create past date to compare with the date of the match
+        const currentDate = new Date();
+        console.log(currentDate)
+        const y = new Date(data.date);
+        if(data.firstTeam==data.secondTeam)
+        {
+            alert("Same team");
+            return;
+        }
+        else if(y<=currentDate)
+        {
+            alert("Past Date");
+            return;
+        }
+        try {
+            // data.seating = Array.from({length: Number(stadiumData[Number(data.stadium)].numSeats)}, (v, i) => 0);
+            // data.noRows = stadiumData[Number(data.stadium)].numRows;
+            // data.stadiumname = stadiumData[Number(data.stadium)].stadium;
+            // data.reserved = 0;
+            const request = await axios.post(routes.addMatch, data, 
+            { headers: { Authorization: `Bearer ${sessionStorage.getItem("tokenValue")}` } }).then((res) => {
+                console.log(res)
+            })
+            window.location.reload(false);
 
-        // } catch (err) {
+        } catch (err) {
             
-        // }
+        }
 
     }
     addMatch()
     };
 
-    const Teams = ['Al Ahly FC','Pyramids FC','Zamalek SC','Cleopatra FC','Modern Future FC','El Masry SC','Bank El Ahly','	Pharco FC','El Mokawloon SC',"Tala'ea El Gaish",'Ittihad Alexandria SC','Smouha SC','Enppi SC','Zed FC','	Ismaily SC','El Gouna FC',
-    'Baladiyat El Mahalla','El Dakhlia SC']
 
-    function dropDownMenu(option,index) {
-    return (
-        <option  key = {index}className={classes.option} value={index}>{option}</option>
-    )
-}
     return (
 
     <div className={classes.body}>
@@ -108,14 +116,14 @@ const AddMatch = (props) => {
             <div className={classes.forminput}>
             <div className={classes.inputbox}>
                 <label className={classes.label}>First Team</label>
-                <Field as="select" className={classes.input} type="text" name="firstTeam" placeholder="Type here">
+                <Field as="select" className={classes.input} type="text" name="firstTeam" placeholder="Type here" required>
                     <option value="" disabled="disabled">Select team</option>
                     {Teams.map((g, index)=> dropDownMenu(g,index))}
                 </Field>
             </div>
             <div className={classes.inputbox}>
                 <label className={classes.label}>Second Team</label>
-                <Field as="select" className={classes.input} type="text" name="secondTeam" placeholder="Type here">
+                <Field as="select" className={classes.input} type="text" name="secondTeam" placeholder="Type here" required>
                     <option value="" disabled="disabled">Select team</option>
                     {Teams.map((g, index)=> dropDownMenu(g,index))}
                 </Field>
@@ -127,11 +135,7 @@ const AddMatch = (props) => {
                 type="date"
                 name="date"
                 placeholder="Type here"
-                />
-                <ErrorMessage
-                className={classes.Err}
-                name="date"
-                component="span"
+                required
                 />
             </div>
             <div className={classes.inputbox}>
@@ -141,18 +145,14 @@ const AddMatch = (props) => {
                 type="time"
                 name="time"
                 placeholder="Type here"
-                />
-                <ErrorMessage
-                className={classes.Err}
-                name="time"
-                component="span"
+                required
                 />
             </div>
             <div className={classes.inputbox}>
                 <label className={classes.label}>Stadium</label>
-                <Field as="select" className={classes.input} type="text" name="venue" placeholder="Type here">
+                <Field as="select" className={classes.input} type="text" name="stadium" placeholder="Type here" >
                 <option value="" disabled="disabled">Select Stadium</option>
-                {stadiumData? stadiumData.map((g, index)=> dropDownMenu(g.venue,index)) : null}
+                {stadiumData? stadiumData.map((g, index)=> dropDownMenu(g.stadium,index)) : null}
                 </Field>
             </div>
             <div className={classes.inputbox}>
@@ -162,11 +162,7 @@ const AddMatch = (props) => {
                 type="text"
                 name="referee"
                 placeholder="Type here"
-                />
-                <ErrorMessage
-                className={classes.Err}
-                name="referee"
-                component="span"
+                required
                 />
             </div>
             <div className={classes.inputbox}>
@@ -176,11 +172,7 @@ const AddMatch = (props) => {
                 type="text"
                 name="firstLinesmen"
                 placeholder="Type here"
-                />
-                <ErrorMessage
-                className={classes.Err}
-                name="firstLinesmen"
-                component="span"
+                required
                 />
             </div>
             <div className={classes.inputbox}>
@@ -190,11 +182,7 @@ const AddMatch = (props) => {
                 type="text"
                 name="secondLinesmen"
                 placeholder="Type here"
-                />
-                <ErrorMessage
-                className={classes.Err}
-                name="secondLinesmen"
-                component="span"
+                required
                 />
             </div>
             </div>
